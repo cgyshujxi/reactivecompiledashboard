569d1fea267a1c824cbdd309c3095f00df8c2888
cgyshujxi <js1floger.h@hotmail.com>
2024-05-06 01:48:22 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-function serialize(root) {
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    if (node) {
-      result.push(node.val);
-      queue.push(node.left, node.right);
-    } else {
-      result.push(null);
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  for (let i = 0; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
     }
   }
-  while (result[result.length - 1] === null) {
-    result.pop();
-  }
-  return result;
+  return Math.max(...dp);
 }
