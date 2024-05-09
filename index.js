f3e20a1f07d1b7c164412c3409d7ccde32e9aa7f
cgyshujxi <js1floger.h@hotmail.com>
2024-05-10 05:37:52 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const flatten = (arr, depth = 1) =>
-  arr.reduce(
-    (a, v) =>
-      a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v),
-    [],
-  );
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
+  }
+  return [...map.values()];
+}
